f9add5c5d8e2e5811cf30777f60d6729e15821eb
AQUINO R. D. G <aquinordga@gmail.com>
2023-01-11 17:07:23 +0000

    added cdr_score

--- src/maul/clustering.py
+++ src/maul/clustering.py
@@ -2,12 +2,13 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.simplefilter(action='ignore', category=UserWarning)
 
-##### Consensus Index #####
-
 from sklearn.cluster import SpectralClustering
 from sklearn.metrics.cluster import adjusted_rand_score
 from numpy.random import RandomState
 import numpy as np
+import pandas as pd
+
+##### Consensus Index #####
 
 def consensus_score(P):
     """
@@ -75,8 +76,6 @@ def wconsensus(P, W, k, random_state=None):
     
 ##### Clustering Utility Based on Averaged information Gain of isolating Each cluster (CUBAGE)#####
 
-import pandas as pd
-
 def entropy_ajusted_arr(arr):
     """
     Parameters:
@@ -141,7 +140,7 @@ def cubage_score(X, labels):
     `labels` array: Labels 
     
     Return:
-    `CUBAGE` float: Entropy
+    `CUBAGE` float: Score
     
     """
     
@@ -174,7 +173,86 @@ def cubage_score(X, labels):
     CUBAGE = AGE/E
     
     return CUBAGE
+    
+##### Contiguous Density Region (CDR) Index #####
 
+# Example
+
+#import matplotlib.pyplot as plt
+
+#X = np.array([[1,1], [2,1], [2,2], [1, 1.5],
+#              [1,6], [2,6], [2, 5.5],
+#              [7,5], [7,6], [8,5], [8,6], [7, 5.5],
+#              [8,1], [8, 1.5],
+#              [4.5,3.5]])
+
+#labels = np.array([0,0,0,0,1,1,1,2,2,2,2,2,3,3,4])
+
+#plt.scatter(X[:,0], X[:,1])
+#plt.grid()
+#plt.show    
+        
+def cdr_score(X, labels):
+    """
+    Parameters:
+    `X` array: Data
+    `labels` array: Labels 
+    
+    Return:
+    `CUBAGE` float: Score
+    
+    """
+
+    assert type(X).__module__ == np.__name__
+    assert type(labels).__module__ == np.__name__
+
+    X = pd.DataFrame(X)
+    k_ = max(labels)+1
+
+    X['k'] = labels
+
+    unif = []
+    nk = []
+
+    for k in range(k_):
+        #print('K:', k)
+        points = X[X.k == k].loc[:, X[X.k == k].columns != 'k'].reset_index(drop=True) #filter the points of a specific cluster
+        nk.append(points.shape[0]) #get the number of examples in the each cluster
+        l = []
+
+        for i in range(len(points)):
+            l_aux = []
+            for j in range(len(points)):
+                if i != j:
+                    l_aux.append(np.linalg.norm(points.loc[i] - points.loc[j])) #euclidean distance for each combination exclusive of points
+            
+            #print('dist:', l_aux)
+            if len(l_aux) > 0:
+                l.append(min(l_aux)) #get local density
+            else:
+                l.append(0)
+                
+            
+
+        avg_den_k = sum(l)/len(l) #get density of clusters
+        #print('local_den:', l)
+        #print('avg_den_k:', avg_den_k)
+        
+        if len(l) == 1:
+            unif.append(0)
+            #print('local_den - avg_den_k:', 0, '\n')
+            
+        else:
+            unif.append(sum([abs(_- avg_den_k) for _ in l])/avg_den_k) #get uniformity of clusters
+            #print('local_den - avg_den_k:', [abs(_- avg_den_k) for _ in l], '\n')
+
+    cdr = sum([nk[i]*unif[i] for i in range(len(unif))])/sum(nk) #get Contiguous Density Region (CDR) index.
+    #print('nk[i]*unif[i]:', [nk[i]*unif[i] for i in range(len(unif))])
+    #print('unif:', unif)
+    #print('nk:', nk, '\n')
+    
+    return cdr    
+  
 ##### Density-core-based Clustering Validation Index (DCVI) #####
 
 def MST(G, show=False): # Prim's Algorithm
