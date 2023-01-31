310313240d447c7b3e9d1283ecb04094720f21ed
AQUINO R. D. G <aquinordga@gmail.com>
2023-01-31 21:04:26 +0000

    CNAK fixed
    random state removed and added k_hat defined by mode of n tests

--- src/maul/__init__.py
+++ src/maul/__init__.py
@@ -291,10 +291,12 @@ def MST(G, show=False): # Prim's Algorithm
 ##### Cluster Number Assisted Kmeans (CNAK) #####
 
 import numpy as np
-from numpy.random import RandomState
+import random
 from .munkres import *
 from sklearn.cluster import KMeans
 import argparse
+
+import statistics
 
 def Bucketization(cluster1,cluster2,indexes,turn,centers):
     if turn==0:
@@ -385,96 +387,82 @@ def weightMatrixUpdated(global_centroids_list,clusters,dd_list,k_centers,avg_sco
     avg_score=avg_score/count
     return avg_score, count, dd_list, k_centers
 
-def CNAK_core(data,gamma,K,random_state):
+def CNAK_core(data,gamma,K):
+
+	T_S=1
+	T_E=5
+	
+	centroids_list=[]
+	for j in range(T_S,T_E):
+		#~~~~~random sampling without repitiion~~~~~~~~
+		#print("int(len(data)*gamma):",int(len(data)*gamma))
+		index=random.sample(range(0, len(data)),int(len(data)*gamma))
+		samples=[]
+		for k in range(int(len(data)*gamma)):
+			temp=data[index[k]]
+			samples.append(temp)
+		#~~~~~~K-means++ on sampled dataset~~~~~~~~~
+		kmeans = KMeans(n_clusters=K,init='k-means++',n_init=20,max_iter=300,tol=0.0001).fit(samples)
+		centroids=kmeans.cluster_centers_
+		centroids_list.append(centroids)	
+	dd_list=[]
+	k_centers=[[] for i in range(len(centroids))]
+	#~~~~~~Computation of CNAK score and forming K buckets with T_E similar  centroids~~~~~~~~
+	avg_score, count, dd_list,k_centers=weightMatrix(centroids_list,dd_list,k_centers)
+	
+	#~~~~~Estimate the value of T ~~~~~~~~~
+	mean=np.mean(dd_list)
+	std=np.std(dd_list)
+	val=(1.414*20*std)/(mean)
+	
+	global_centroids_list=[]
+	for centroids in (centroids_list):
+		global_centroids_list.append(centroids)
+	centers=[[] for i in range(len(centroids_list[0]))]
+	
+	#~~~~~Repeat untill  T_E > T_threshold ~~~~~~~~~
+	while val>T_E:
+		T_S=T_E
+		T_E=T_E+1
+		centroids_list=[]
+		for j in range(T_S,T_E):
+			index=random.sample(range(0, len(data)),int(len(data)*gamma))
+			datax=[]
+			
+			for k in range(int(len(data)*gamma)):
+				temp=data[index[k]]
+				datax.append(temp)
+			
+			kmeans = KMeans(n_clusters=K,init='k-means++',n_init=20,max_iter=300,tol=0.0001).fit(datax)
+			centroids=kmeans.cluster_centers_
+			centroids_list.append(centroids)
+	
+		avg_score, count, dd_list,k_centers=weightMatrixUpdated(global_centroids_list,centroids_list,dd_list,k_centers,avg_score,count)
+		for centroids in ((centroids_list)):
+			global_centroids_list.append(centroids)
+		mean=np.mean(dd_list)
+		std=np.std(dd_list)
+		val=(1.414*20*std)/(mean)
+		
+	clusterCenterAverage=[]
+	for i in range(len(k_centers)):
+		  clusterCenterAverage.append(np.mean(k_centers[i],axis=0))
+	return val, T_E, avg_score, clusterCenterAverage
+
+def CNAK(data, gamma=0.7, k_min=2, k_max=21, n_tests=50):
+
+    res=[]
     
-    if random_state is None:
-        random_state = RandomState()
-    elif type(random_state) == int:
-        random_state = RandomState(random_state)
-    else:
-        assert type(random_state) == RandomState
+    for n in range(n_tests):
+        CNAK_score=[]
+        k_max_centers=[]
 
-    T_S=1
-    T_E=5
+        for K in range(k_min,k_max):
+            val, T_E, avg_score, k_centers=CNAK_core(data,gamma,K)
+            CNAK_score.append(avg_score)
+            k_max_centers.append(k_centers)
 
-    centroids_list=[]
-    for j in range(T_S,T_E):
-        #~~~~~random sampling without repitiion~~~~~~~~
-        #print("int(len(data)*gamma):",int(len(data)*gamma))
-        index=random_state.choice(range(0, len(data)),int(len(data)*gamma))
-        samples=[]
-        for k in range(int(len(data)*gamma)):
-            temp=data[index[k]]
-            samples.append(temp)
-        #~~~~~~K-means++ on sampled dataset~~~~~~~~~
-        kmeans = KMeans(n_clusters=K,init='k-means++',n_init=20,max_iter=300,tol=0.0001).fit(samples)
-        centroids=kmeans.cluster_centers_
-        centroids_list.append(centroids)	
-    dd_list=[]
-    k_centers=[[] for i in range(len(centroids))]
-    #~~~~~~Computation of CNAK score and forming K buckets with T_E similar  centroids~~~~~~~~
-    avg_score, count, dd_list,k_centers=weightMatrix(centroids_list,dd_list,k_centers)
-
-    #~~~~~Estimate the value of T ~~~~~~~~~
-    mean=np.mean(dd_list)
-    std=np.std(dd_list)
-    val=(1.414*20*std)/(mean)
-
-    global_centroids_list=[]
-    for centroids in (centroids_list):
-        global_centroids_list.append(centroids)
-    centers=[[] for i in range(len(centroids_list[0]))]
-
-    #~~~~~Repeat untill  T_E > T_threshold ~~~~~~~~~
-    while val>T_E:
-        T_S=T_E
-        T_E=T_E+1
-        centroids_list=[]
-        for j in range(T_S,T_E):
-            index=random_state.choice(range(0, len(data)),int(len(data)*gamma))
-            datax=[]
-
-            for k in range(int(len(data)*gamma)):
-                temp=data[index[k]]
-                datax.append(temp)
-
-            kmeans = KMeans(n_clusters=K,init='k-means++',n_init=20,max_iter=300,tol=0.0001).fit(datax)
-            centroids=kmeans.cluster_centers_
-            centroids_list.append(centroids)
-
-        avg_score, count, dd_list,k_centers=weightMatrixUpdated(global_centroids_list,centroids_list,dd_list,k_centers,avg_score,count)
-        for centroids in ((centroids_list)):
-            global_centroids_list.append(centroids)
-        mean=np.mean(dd_list)
-        std=np.std(dd_list)
-        val=(1.414*20*std)/(mean)
-
-    clusterCenterAverage=[]
-    for i in range(len(k_centers)):
-          clusterCenterAverage.append(np.mean(k_centers[i],axis=0))
-    return val, T_E, avg_score, clusterCenterAverage
-
-def CNAK(data, gamma=0.7, k_min=2, k_max=21, random_state=None, show_scores=False):
-    
-    if random_state is None:
-        random_state = RandomState()
-    elif type(random_state) == int:
-        random_state = RandomState(random_state)
-    else:
-        assert type(random_state) == RandomState
-
-    CNAK_score=[]
-    k_max_centers=[]
-
-    for K in range(k_min,k_max):
-        val, T_E, avg_score, k_centers=CNAK_core(data,gamma,K,random_state)
-        CNAK_score.append(avg_score)
-        k_max_centers.append(k_centers)
-
-    K_hat=CNAK_score.index(min(CNAK_score))
-    
-    if show_scores == True:
-        return K_hat+k_min, CNAK_score
-    
-    else:
-        return K_hat+k_min
+        K_hat=CNAK_score.index(min(CNAK_score))
+        res.append(K_hat+k_min)
+        
+    return statistics.mode(res)
